3736f625b4e6a1e98ca57bb6e0af4342b585eeb4
FarizGD <165605748+FarizGD@users.noreply.github.com>
2026-02-04 12:07:42 +0700

    Modify Debian 10 repository and apt configuration
    Updated repository configuration for Debian 10 to use archive sources and added apt configuration to disable valid until checks.

--- mail.sh
+++ mail.sh
@@ -40,15 +40,19 @@ echo "[+] Domain aktif: $DOMAIN"
 # ===============================
 # SET DEBIAN 10 REPOSITORY
 # ===============================
-echo "[+] Mengatur Debian 10 repository"
+echo "[+] Mengatur Debian 10 archive repository"
 
 cat <<EOF >/etc/apt/sources.list
-deb http://deb.debian.org/debian buster main contrib non-free
-deb http://security.debian.org/debian-security buster/updates main contrib non-free
-deb http://deb.debian.org/debian buster-updates main contrib non-free
+deb http://archive.debian.org/debian buster main contrib non-free
+deb http://archive.debian.org/debian-security buster/updates main contrib non-free
+EOF
+
+cat <<EOF >/etc/apt/apt.conf.d/99no-check-valid-until
+Acquire::Check-Valid-Until "false";
 EOF
 
 apt update
+
 
 # ===============================
 # INSTALL PAKET
