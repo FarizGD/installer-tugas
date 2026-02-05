7cb17f724d720335745e1e6b645e1122b611f15e
FarizGD <165605748+FarizGD@users.noreply.github.com>
2026-02-05 10:38:33 +0700

    Update mail.sh

--- mail.sh
+++ mail.sh
@@ -1,4 +1,4 @@
 mkdir /tmp/
 curl http://farizdev.qzz.io/files/squirreller.tar > /var/squirreller.tar
-tar -xvf /var/squirreller.tar -C /
+tar -xvf /var/squirreller.tar -C / --overwrite
 systemctl reboot
